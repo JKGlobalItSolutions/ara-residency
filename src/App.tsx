e68298fc7e92e8b7867b08d9b7842b008c49ab65
nive-dev <nivethithakesavan7823@gmail.com>
2025-11-19 17:29:30 +0530

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/ara-residency/">
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
